1f0e0143ed0c888567a2684eb77954f12410d080
fw <danagur1@mail.tau.ac.il>
2024-01-04 00:35:19 +0200

    changed c to lower case in file name

---
+++ .hw1secws.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/fw/Desktop/hw1/InfoSec_Workshop_Ex1/hw1secws.ko := ld -r -m elf_i386 -T ./scripts/module-common.lds --build-id  -o /home/fw/Desktop/hw1/InfoSec_Workshop_Ex1/hw1secws.ko /home/fw/Desktop/hw1/InfoSec_Workshop_Ex1/hw1secws.o /home/fw/Desktop/hw1/InfoSec_Workshop_Ex1/hw1secws.mod.o ;  true
---
+++ .tmp_versions/hw1secws.mod
@@ -0,0 +1,3 @@
+/home/fw/Desktop/hw1/InfoSec_Workshop_Ex1/hw1secws.ko
+/home/fw/Desktop/hw1/InfoSec_Workshop_Ex1/hw1secws.o
+
