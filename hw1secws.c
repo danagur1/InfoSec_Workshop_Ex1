8060475e1b4b374df96dcf745fc470f43303a47f
fw <danagur1@mail.tau.ac.il>
2024-01-05 00:40:14 +0200

    changed hooknum to 2 and removed unneccery lines

--- hw1secws.c
+++ hw1secws.c
@@ -1,7 +1,10 @@
 #include <linux/module.h>	/* Needed by all modules */
 #include <linux/kernel.h>	/* Needed for KERN_INFO and for the 						Macros */
+
 #include <linux/netfilter.h>
 #include <linux/netfilter_ipv4.h>
+//#include <linux/ip.h>
+//#include <linux/config.h>
 
 unsigned int my_nf_hookfn(void *priv, struct sk_buff *skb, const struct nf_hook_state *state){
 	return NF_DROP;
@@ -9,15 +12,10 @@ unsigned int my_nf_hookfn(void *priv, struct sk_buff *skb, const struct nf_hook_
 
 static int __init my_module_init_function(void) {
 	struct nf_hook_ops my_nf_hook_ops;
-	//my_nf_hook_ops.list = {NULL, NULL};
 	my_nf_hook_ops.hook = &my_nf_hookfn;
-	//my_nf_hook_ops.flush = NULL;
 	my_nf_hook_ops.pf = PF_INET;
-	my_nf_hook_ops.hooknum = NF_IP_FORWARD;
+	my_nf_hook_ops.hooknum = 2; //NF_IP_FORWARD
 	printk(KERN_INFO "Hello World!\n");
-	 //= {{NULL, NULL}, &my_nf_hookfn, NULL, PF_INIT, NF_IP_FORWARD}; 
-	
-	
 
 	return 0; /* if non-0 return means init_module failed */
 }
