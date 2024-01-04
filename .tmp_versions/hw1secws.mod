cf1ed2d2c59d817c38e667248cef121bc3d04969
fw <danagur1@mail.tau.ac.il>
2024-01-05 00:01:16 +0200

    added nf_hook_ops object but error on NF_IP_PRE_ROUTING

--- .tmp_versions/hw1secws.mod
+++
@@ -1,3 +0,0 @@
-/home/fw/Desktop/hw1/InfoSec_Workshop_Ex1/hw1secws.ko
-/home/fw/Desktop/hw1/InfoSec_Workshop_Ex1/hw1secws.o
-
--- hw1secws.c
+++ hw1secws.c
@@ -1,8 +1,24 @@
 #include <linux/module.h>	/* Needed by all modules */
 #include <linux/kernel.h>	/* Needed for KERN_INFO and for the 						Macros */
+#include <linux/netfilter.h>
+#include <linux/netfilter_ipv4.h>
+
+unsigned int my_nf_hookfn(void *priv, struct sk_buff *skb, const struct nf_hook_state *state){
+	return NF_DROP;
+}
 
 static int __init my_module_init_function(void) {
+	struct nf_hook_ops my_nf_hook_ops;
+	//my_nf_hook_ops.list = {NULL, NULL};
+	my_nf_hook_ops.hook = &my_nf_hookfn;
+	//my_nf_hook_ops.flush = NULL;
+	my_nf_hook_ops.pf = PF_INET;
+	my_nf_hook_ops.hooknum = NF_IP_FORWARD;
 	printk(KERN_INFO "Hello World!\n");
+	 //= {{NULL, NULL}, &my_nf_hookfn, NULL, PF_INIT, NF_IP_FORWARD}; 
+	
+	
+
 	return 0; /* if non-0 return means init_module failed */
 }
 static void __exit my_module_exit_function(void) {
