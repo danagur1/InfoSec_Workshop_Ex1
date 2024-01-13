4760a2062c7c89cdb2b537f41713a570746f9d4c
danagur1 <danagur1@mail.tau.ac.il>
2024-01-13 19:22:56 +0200

    added handeling errors

--- hw1secws.c
+++ hw1secws.c
@@ -20,35 +20,44 @@ unsigned int accept_hookfn(void *priv, struct sk_buff *skb, const struct nf_hook
 	return NF_ACCEPT;
 }
 
-static void set_forward_hook(void){
+static int set_forward_hook(void){
 	forward_nh_ops.hook = &drop_hookfn;
 	forward_nh_ops.pf = PF_INET;
 	forward_nh_ops.hooknum = NF_INET_FORWARD;
 	forward_nh_ops.priority = NF_IP_PRI_FIRST;
-	nf_register_net_hook(&init_net, &forward_nh_ops);
+	return nf_register_net_hook(&init_net, &forward_nh_ops);
 }
 
-static void set_input_hook(void){
+static int set_input_hook(void){
 	input_nh_ops.hook = &accept_hookfn;
 	input_nh_ops.pf = PF_INET;
 	input_nh_ops.hooknum = NF_INET_LOCAL_IN;
 	input_nh_ops.priority = NF_IP_PRI_FIRST;
-	nf_register_net_hook(&init_net, &input_nh_ops);
+	return nf_register_net_hook(&init_net, &input_nh_ops);
 }
 
-static void set_output_hook(void){
+static int set_output_hook(void){
 	output_nh_ops.hook = &accept_hookfn;
 	output_nh_ops.pf = PF_INET;
 	output_nh_ops.hooknum = NF_INET_LOCAL_OUT;
 	output_nh_ops.priority = NF_IP_PRI_FIRST;
-	nf_register_net_hook(&init_net, &output_nh_ops);
+	return nf_register_net_hook(&init_net, &output_nh_ops);
 }
 
 static int __init my_module_init_function(void) {
-	set_forward_hook();
-	set_input_hook();
-	set_output_hook();
-	return 0; /* if non-0 return means init_module failed */
+	int return_code = set_forward_hook();
+	if (return_code !=0){
+		return return_code; //if registration failed, return error
+	}
+	return_code = set_input_hook();
+	if (return_code !=0){
+		return return_code; //if registration failed, return error
+	}
+	return_code = set_output_hook()
+	if (return_code !=0){
+		return return_code; //if registration failed, return error
+	}
+	return 0; //registration succeeded
 }
 static void __exit my_module_exit_function(void) {
 	nf_unregister_net_hook(&init_net, &forward_nh_ops);
